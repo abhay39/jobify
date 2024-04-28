0978d55eca0f351b4475da215ecdc615e66248c2
abhay39 <abhayguptaak39@gmail.com>
2024-04-28 15:42:08 +0545

    final

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env:{
-        API:'http://localhost:5000/api/v1'
+        API:'https://jobifybackend.vercel.app/api/v1'
     },
     images:{
         domains:['res.cloudinary.com']
